76fd0cf17a610fcdbc519757b2e62493951bfd72
glucrypto <glucrypto@protonmail.com>
2024-10-22 08:23:44 -0400

    fix: added drizzle

---
+++ drizzlestudio/index.ts
@@ -0,0 +1,7 @@
+// drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  dbCredentials: {
+    url: "postgresql://postgres:WVOJZNqbjrHSZVrvAmuQDYcjuFkFwQjw@autorack.proxy.rlwy.net:20050/railway"
+  },
+});
